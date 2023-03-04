b80586893fb0eabf2d11837cef0b43a195dc3942
cqtqlyst <aayush.gaywala@gmail.com>
2023-03-04 09:37:21 -0800

    Update Word.cs

--- Sticky-Keys/Assets/Materials/Classes/Word.cs
+++ Sticky-Keys/Assets/Materials/Classes/Word.cs
@@ -1,13 +1,30 @@
-using Systems.Collections;
+using System.Collections;
 
 public class Word
 {
     
-    public ArrayList wordArrList = new ArrayList();
-    public String wordStr;
+    // fields
+
+    public ArrayList wordArrList;
+    public string wordStr;
+
+    // constructors
 
     public Word() {
-        
+        wordArrList = new ArrayList();
+        wordStr = "";
+    }
+
+    // methods
+
+    public void addLetter(Letter l) {
+        wordArrList.Add(l);
+        wordStr += l.letter;
+    }
+
+    public void addLetter(char c) {
+        wordArrList.Add(new Letter(c));
+        wordStr += c;
     }
 
 }
