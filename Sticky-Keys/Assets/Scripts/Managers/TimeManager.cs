8856c28425cdb2a54ab7f6b48fc394c468571700
cqtqlyst <aayush.gaywala@gmail.com>
2023-03-10 22:22:33 -0800

    working on stuff

--- Sticky-Keys/Assets/Scripts/Managers/TimeManager.cs
+++ Sticky-Keys/Assets/Scripts/Managers/TimeManager.cs
@@ -1,38 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Timers;
-using TMPro;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using System.Timers;
+//using TMPro;
 
-public class TimerManager : MonoBehaviour
-{
+//public class TimerManager : MonoBehaviour
+//{
 
-    public int seconds = 120;
-    private System.Timers.Timer timer;
-    public int timeBetweenCoolDown = 1000;
+//    public int seconds = 120;
+//    private System.Timers.Timer timer;
+//    public int timeBetweenCoolDown = 1000;
 
-    public TextMeshPro text;
-
-
-    void Start()
-    {
-        timer = new Systems.Timers.Timer(timeBetweenCoolDown);
-        Timer.Elapsed += Timer_Elapsed;
-        Timer.Enabled = true;
-        Timer.AutoReset = true;
-        Timer.Start();
-    }
-
-    private void Timer_Elapsed(object sender, ElapsedEventArgs e)
-    {
-        seconds--;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+//    public TextMeshPro text;
 
 
+//    void Start()
+//    {
+//        timer = new Systems.Timers.Timer(timeBetweenCoolDown);
+//        Timer.Elapsed += Timer_Elapsed;
+//        Timer.Enabled = true;
+//        Timer.AutoReset = true;
+//        Timer.Start();
+//    }
 
-    }
-}
+//    private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+//    {
+//        seconds--;
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+
+
+//    }
+//}
