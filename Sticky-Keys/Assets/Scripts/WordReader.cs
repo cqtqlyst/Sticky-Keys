af2d56f123e60f888362acab9b50036251e96c40
sonicunleshed21 <rishboss0@gmail.com>
2023-04-21 11:01:01 -0700

    made working onscreen text box for phase 2 as well as working word finder and dictionary from letters a to c

--- Sticky-Keys/Assets/Scripts/WordReader.cs
+++ Sticky-Keys/Assets/Scripts/WordReader.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // reads the word list in for the word.cs checking isLetter
-public class WordReader
+public class WordReader : MonoBehaviour
 {
-
+    private int[] letters = new int[26];
+    private string word;
+    private int letterNum;
+    private int wordCount;
+    Dictionary<string, int> WordBank;
+    [SerializeField] private TextAsset file;
     void Start() {
 
-        string[] words = System.IO.File.ReadAllLines(@"Assets\Words\words.txt");
-
+        WordBank = new Dictionary<string, int>();
+        ReadFile();
+        Debug.Log(wordCount);
+    }
+    void ReadFile()
+    {
+        var splitFile = new string[] { "\r\n", "\r", "\n" };
+        var Lines = file.text.Split(splitFile, System.StringSplitOptions.RemoveEmptyEntries);
+        wordCount = Lines.Length;
+        for(int i = 0; i < wordCount; i++)
+        {
+            int length = Lines[i].Length;
+            WordBank.Add(Lines[i], length);
+        }
     }
 
+    void Update()
+    {
+       
+    }
+
+    public void ReadStringInput(string input)
+    {
+        word = input;
+        Debug.Log(word);
+    }
 }
