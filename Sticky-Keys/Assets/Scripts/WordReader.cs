38a53cc2fad2db09fdce4f8ea3b1a34e2ca4349d
sonicunleshed21 <rishboss0@gmail.com>
2023-04-22 20:07:09 -0700

    finished phase 2 entirely
    Kevin was here

--- Sticky-Keys/Assets/Scripts/WordReader.cs
+++ Sticky-Keys/Assets/Scripts/WordReader.cs
@@ -8,21 +8,35 @@ public class WordReader : MonoBehaviour
 {
     private int[] letters = new int[26];
     private string word;
+    private string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private string letterPrint;
     private int letterNum;
     private int wordCount;
     private int score = 0;
+    private bool isValid;
 
     Dictionary<string, int> WordBank;
     [SerializeField] private TextAsset file;
 
     public TMP_Text Points;
+    public TMP_Text validLetters;
 
     void Start() {
         WordBank = new Dictionary<string, int>();
         ReadFile();
         Debug.Log(wordCount);
-        
+        for (int i = 0; i < 26; i++)
+        {
+            letters[i] = 2;
+            for (int j = 0; j < letters[i]; j++)
+            {
+                letterPrint += alphabet[i];
+            }
+            letterPrint += "   ";
+        }
+        validLetters.text = letterPrint;
     }
+
     void ReadFile()
     {
         var splitFile = new string[] { "\r\n", "\r", "\n" };
@@ -40,12 +54,43 @@ public class WordReader : MonoBehaviour
     {
         word = input;
         Debug.Log(word);
-        if (WordBank.ContainsKey(word))
+        int length = word.Length;
+        isValid = true;
+        letterPrint = "";
+        for (int i = 0; i < length; i++)
         {
-            score += WordBank[word];
-            Debug.Log(score);
-            Points.text = "Score: " + score;
+            int a = word[i];
+            a -= 97;
+            if (letters[a] <= 0)
+            {
+                isValid = false;
+            }
+            else
+            {
+                letters[a]--;
+            }
         }
-        
+        if (isValid == true)
+        {
+            if (WordBank.ContainsKey(word))
+            {
+                score += WordBank[word];
+                Debug.Log(score);
+                Points.text = "Score: " + score;
+            }
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            for (int j = 0; j < letters[i]; j++)
+            {
+                letterPrint += alphabet[i];
+            }
+            if (letters[i] != 0)
+            {
+                letterPrint += " ";
+            }
+        }
+        validLetters.text = letterPrint;
+
     }
 }
