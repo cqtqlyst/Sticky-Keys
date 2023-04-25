8bc9e026c0ce1d820995885f574fb1fd3636f71b
sonicunleshed21 <rishboss0@gmail.com>
2023-04-24 22:59:41 -0700

    added detailed comments to wordreader and removed unnecessary files

--- Sticky-Keys/Assets/Scripts/WordReader.cs
+++ Sticky-Keys/Assets/Scripts/WordReader.cs
@@ -6,22 +6,35 @@ using TMPro;
 // reads the word list in for the word.cs checking isLetter
 public class WordReader : MonoBehaviour
 {
-    private static int[] letters = new int[26];
+    //creating global letters to be acessed from any file
+    public static int[] letters = new int[26];
+    // creating private variables for:
+    // the word inputted by the player
+    // initialize the alphabet for converting letters to numbers
+    // initialize string for printing on screen
+    // implements amount of words in the dictionary
+    // creates booleans to see if words inputted by the player is legal
     private string word;
     private string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private string letterPrint;
     private int wordCount;
     private bool isValid;
 
+    // initializes a dictionary where all of the words are stored
+    // initialize the file asset in our game
     Dictionary<string, int> WordBank;
     [SerializeField] private TextAsset file;
 
+    // creates on screen text boxes
     public TMP_Text Points;
     public TMP_Text validLetters;
 
     void Start() {
+        // intializes the dictionary
         WordBank = new Dictionary<string, int>();
+        // reads file and adds words to dictionary
         ReadFile();
+        // prints word counts for testing
         Debug.Log(wordCount);
         for (int i = 0; i < 26; i++)
         {
@@ -38,6 +51,7 @@ public class WordReader : MonoBehaviour
 
     void ReadFile()
     {
+        // reads file and splits file by new line to save words and their points 
         var splitFile = new string[] { "\r\n", "\r", "\n" };
         var Lines = file.text.Split(splitFile, System.StringSplitOptions.RemoveEmptyEntries);
         wordCount = Lines.Length;
@@ -48,20 +62,28 @@ public class WordReader : MonoBehaviour
         }
     }
 
-
+    // method read every time player inputs a word and presses enter on screen
     public void ReadStringInput(string input)
     {
+        // word that the player inputs
         word = input;
+        // implements word to lowercase and removes spaces for easier player aces
         word = word.ToLower();
         word = word.Replace(" ", "");
+        // prints word for testing
         Debug.Log(word);
+        // finds the length of the word to test each individual letter
         int length = word.Length;
+        // the word starts as valid everytime 
         isValid = true;
+        // initializing the print screen for the available letters
         letterPrint = "";
         for (int i = 0; i < length; i++)
         {
+            //finds the integer value of each character
             int a = word[i];
             a -= 97;
+            // if the letter is invalid the word will become false and the player won't recive any points
             if (letters[a] <= 0)
             {
                 isValid = false;
@@ -71,15 +93,21 @@ public class WordReader : MonoBehaviour
                 letters[a]--;
             }
         }
+        // checks if the word is still valid 
         if (isValid == true)
         {
+            // checks if the word actually exists
             if (WordBank.ContainsKey(word))
             {
+                //adds score since the word passes all checks
                 ScoreManager.score += WordBank[word];
+                // outputs player score to make sure bugs don't exist
                 Debug.Log(ScoreManager.score);
+                // prints score on screen
                 Points.text = "Score: " + ScoreManager.score;
             }
         }
+        // runs loop to print remaining letters on screen for the player
         for (int i = 0; i < 26; i++)
         {
             for (int j = 0; j < letters[i]; j++)
