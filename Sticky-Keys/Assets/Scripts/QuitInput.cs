4f8b7aedbe96c57a612f8eff832569137706f20c
cqtqlyst <aayush.gaywala@gmail.com>
2023-04-27 00:49:19 -0700

    Revert "updated color + quit input"
    This reverts commit 03fde8c8708f147a5afa507c42eed94ad37ff9df.

--- Sticky-Keys/Assets/Scripts/QuitInput.cs
+++ Sticky-Keys/Assets/Scripts/QuitInput.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class QuitInput : MonoBehaviour
 {
@@ -10,11 +9,6 @@ public class QuitInput : MonoBehaviour
         if (Input.GetButtonDown("Cancel"))
         {
             Application.Quit();
-        }
-
-        if (Input.GetButtonDown("Enable Debug Button 2"))
-        {
-            SceneManager.LoadScene("Phase2");
-        }
+        }    
     }
 }
