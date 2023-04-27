53fd39b026432026837e52915815cf3c1ed5b828
cqtqlyst <aayush.gaywala@gmail.com>
2023-04-26 23:11:57 -0700

    Update LeaderBoardManager.cs

--- Sticky-Keys/Assets/Scripts/Managers/LeaderBoardManager.cs
+++ Sticky-Keys/Assets/Scripts/Managers/LeaderBoardManager.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class LeaderBoardManager : MonoBehaviour
 {
+
+
     public void ReadStringInput(string input)
     {
-        Debug.Log(input);
+        writeFile(input, ScoreManager.score);
+    }
+
+    void writeFile(string name, int score)
+    {
+        string path = "/Assets/Files/leaderboard.txt";
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine("Test");
     }
 }
