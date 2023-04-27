c71b47a13210136ac02717a673476d1492153292
sonicunleshed21 <rishboss0@gmail.com>
2023-04-26 23:38:00 -0700

    added instructions button and changed titlescreen folder to buttons folder

--- Sticky-Keys/Assets/Scripts/Managers/ScoreManager.cs
+++ Sticky-Keys/Assets/Scripts/Managers/ScoreManager.cs
@@ -5,13 +5,15 @@ public class ScoreManager : MonoBehaviour
 {
     public TMP_Text scoreDisplay;
     public static int score;
+    public static int totalScore;
     private void Start()
     {
         scoreDisplay.text = "You Got a score of: " + score;
         if (score > 0)
         {
             scoreDisplay.text = "You Got a score of: " + score + "!" + "\r\n" + "You were able to make: " + WordReader.wordsCreated;
-        } 
+        }
+        totalScore += score;
     }
 
 }
